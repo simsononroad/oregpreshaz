9a60ba2c83ddb533e9690e1dd760222424bc07f4
simsononroad <d-github@kopaszhegy.hu>
2024-04-14 18:09:38 +0200

    Elso verzio

---
+++ op.gyuris.hu/op.gyuris.hu/aloldalak/main.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+	const toggleButton = document.getElementsByClassName('toggle-button')[0]
+	const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+	toggleButton.addEventListener('click', () => {
+		navbarLinks.classList.toggle('active')
+	})
+})
