4336cc42f67c85cebc10d6c5b5b5743d5accaf2e
simsononroad <d-github@kopaszhegy.hu>
2024-04-19 17:44:51 +0200

    brand-title-fixed

--- op.gyuris.hu/op.gyuris.hu/aloldalak/main.js
+++ op.gyuris.hu/op.gyuris.hu/aloldalak/main.js
@@ -1,8 +1,12 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 	const toggleButton = document.getElementsByClassName('toggle-button')[0]
 	const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+	const BrandTitle = document.getElementsByClassName('brand-title')[0]
 
 	toggleButton.addEventListener('click', () => {
 		navbarLinks.classList.toggle('active')
 	})
+	BrandTitle.addEventListener('click', () => {
+        window.open("../index.html")
+    })
 })
