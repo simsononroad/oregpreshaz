c6ac71ea82ba1932df860837d0480b86e96e0211
simsononroad <d-github@kopaszhegy.hu>
2024-04-22 15:08:52 +0200

    de/en-navbar-fix

--- op.gyuris.hu/op.gyuris.hu/aloldalak/main.js
+++ op.gyuris.hu/op.gyuris.hu/aloldalak/main.js
@@ -1,11 +1,14 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-	const toggleButton = document.getElementsByClassName('toggle-button')[0]
-	const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 	const BrandTitle = document.getElementsByClassName('brand-title')[0]
 
-	toggleButton.addEventListener('click', () => {
-		navbarLinks.classList.toggle('active')
-	})
+	for (let toggleButton of document.querySelectorAll('.toggle-button')) {
+		toggleButton.addEventListener('click', () => {
+			for (let navbarLinks of document.querySelectorAll('.navbar-links')) {
+				navbarLinks.classList.toggle('active')
+			}
+		})
+	}
+
 	BrandTitle.addEventListener('click', () => {
         window.open("../index.html")
     })
